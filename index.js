639f1c165a51cd18505e102853b6372618508392
Gino A. Limo <ginoalgarcia@gmail.com>
2022-09-03 13:15:13 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+import express from "express";
+import fileUpload from "express-fileupload";
+import { uploadFile, getFiles, getFile, downloadFile, getFileURL } from "./s3.js";
+
+const app = express();
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "./uploads",
+  })
+);
+
+app.get("/files", async (req, res) => {
+  const result = await getFiles();
+
+  res.json(result.Contents);
+});
+
+app.get("/files/:fileName", async (req, res) => {
+  const result = await getFileURL(req.params.fileName);
+  res.json({
+    url: result
+  });
+});
+
+app.get("/download/:fileName", async (req, res) => {
+     await downloadFile(req.params.fileName);
+    res.json({message: "downloaded"});
+  });
+
+app.post("/files", async (req, res) => {
+  const result = await uploadFile(req.files.file);
+  res.json({ result });
+});
+
+app.use(express.static('images'))
+
+app.listen(3000);
+console.log(`Server on port ${3000}`);
